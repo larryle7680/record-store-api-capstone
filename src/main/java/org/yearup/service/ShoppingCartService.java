ec34848f61f568b72ba96ed70972dc1585eeb97c
Larry <larryle7680@gmail.com>
2026-06-23 11:46:54 -0500

    Added Logic to getByUserId() - This method loads all the items that has been added to the unique userId.
    Made a create method for addToCart().

--- src/main/java/org/yearup/service/ShoppingCartService.java
+++ src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,8 +1,13 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
+import org.yearup.models.CartItem;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.repository.ShoppingCartRepository;
+
+import java.util.List;
 
 @Service
 public class ShoppingCartService
@@ -19,9 +24,29 @@ public class ShoppingCartService
 
     public ShoppingCart getByUserId(int userId)
     {
-        // load the user's cart rows, look up each product, and build the ShoppingCart
-        return null;
+        // load the user's cart rows
+        List<CartItem> items = shoppingCartRepository.findByUserId(userId);
+        ShoppingCart cart = new ShoppingCart();
+         //look up each product, and build the ShoppingCart
+        for(CartItem cartItem : items){
+
+            //Getting the products ID from the cartItem and storing it individually as products
+            Product products = productService.getById(cartItem.getProductId());
+            //Create an empty Object
+            ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
+            shoppingCartItem.setProduct(products);
+            shoppingCartItem.setQuantity(cartItem.getQuantity());
+
+            cart.add(shoppingCartItem);
+        }
+
+        return cart;
     }
 
     // add additional methods here
+    //Creating a cartItem to add to cart
+    //Use CartItem because I'm trying to save individual things into a collection
+    public CartItem addToCart(CartItem cartItem){
+        return shoppingCartRepository.save(cartItem);
+    }
 }
